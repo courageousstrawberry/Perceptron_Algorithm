c4de7451acb78d38893e4298dcd2d2c0ef82c6cd
courageousstrawberry <elech2a@gmail.com>
2025-06-05 11:54:42 -0700

    Add all functions

--- Perceptron.py
+++ Perceptron.py
@@ -1,5 +1,14 @@
 import numpy as np
 class Perceptron:
 
-    def __init__(self):
+    def __init__(self, eta=0.01, n_iter=50, random_state=1):
+        pass
+
+    def fit(self, X, y):
+        pass
+
+    def net_input(self, X):
+        pass
+
+    def predict(self, X):
         pass
