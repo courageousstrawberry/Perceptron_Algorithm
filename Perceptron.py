b145c58afeb2c4c760a87cd81422b2f6f6ca782d
courageousstrawberry <elech2a@gmail.com>
2025-06-06 11:36:15 -0700

    Add comments

--- Perceptron.py
+++ Perceptron.py
@@ -1,8 +1,24 @@
 import numpy as np
 class Perceptron:
+    """Perceptron classifier.
 
+    Parameters
+    ----------
+    eta : float
+        Learning rate (between 0.0 and 1.0)
+    n_iter : int
+        Passes over the training dataset.
+    random_state : int
+        Random number generator seed for random weight initialization.
+
+    Attributes
+    ----------
+    
+    """
     def __init__(self, eta=0.01, n_iter=50, random_state=1):
-        pass
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
 
     def fit(self, X, y):
         pass
