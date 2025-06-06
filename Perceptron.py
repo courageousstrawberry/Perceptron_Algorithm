5512c0ef5999f992b9e4da727ab80a3ec880de90
courageousstrawberry <elech2a@gmail.com>
2025-06-06 11:48:41 -0700

    complete fit method

--- Perceptron.py
+++ Perceptron.py
@@ -21,7 +21,35 @@ class Perceptron:
         self.random_state = random_state
 
     def fit(self, X, y):
-        pass
+        """Fit training data.
+        
+        Parameters
+        ----------
+        X: {array-like}, shape = [n_examples, n_features]
+            Training vectors, where n_examples is the number
+            of examples and n_features is the number of features.
+        y: array-like, shape = [n_examples]
+            Target values.
+        
+        Returns
+        -------
+        self : object
+
+        """
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=X.shape[1])
+        self.b_ = np.float_(0.)
+        self.errors_ = []
+
+        for _ in range(self.n_iter):
+            errors = 0
+            for xi, target in zip(X, y):
+                update = self.eta * (target - self.predict(xi))
+                self.w_ += update * xi
+                self.b_ += update
+                errors += int(update != 0.0)
+            self.errors_.append(errors)
+        return self
 
     def net_input(self, X):
         pass
